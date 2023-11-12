8e89c8e4fa099821c7a553f645233d370f945ebf
jjoonior <jjoonior97@gmail.com>
2023-11-13 00:37:28 +0900

    Feat: UserEntity 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './users/user.entity';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
       isGlobal: true,
+    }),
+    TypeOrmModule.forFeature([UserEntity]),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [UserEntity],
+      // 개발환경은 true
+      synchronize: true,
     }),
   ],
   controllers: [AppController],
---
+++ src/users/user.entity.ts
@@ -0,0 +1,45 @@
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  Unique,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+@Unique(['email', 'nickname'])
+export class UserEntity extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({
+    type: 'varchar',
+    length: 255,
+  })
+  email: string;
+
+  @Column({
+    type: 'varchar',
+    length: 255,
+  })
+  password: string;
+
+  @Column({
+    type: 'varchar',
+    length: 255,
+  })
+  nickname: string;
+
+  @Column({
+    type: 'int',
+  })
+  language: number;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+}
