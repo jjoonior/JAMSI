19f64fef6479f8de2116480cf67d8d65645bd0fa
jjoonior <jjoonior97@gmail.com>
2023-11-15 21:55:56 +0900

    Move: auth.guard.ts 파일 위치 변경

--- src/auth/guard/auth.guard.ts
+++ src/auth/guard/auth.guard.ts
@@ -4,7 +4,7 @@ import {
   ExecutionContext,
   UnauthorizedException,
 } from '@nestjs/common';
-import { AuthService } from './auth/auth.service';
+import { AuthService } from '../auth.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
