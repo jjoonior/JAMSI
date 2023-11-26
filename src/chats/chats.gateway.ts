799589735102ab4b118e4f10d7dd73523919fa41
jjoonior <jjoonior97@gmail.com>
2023-11-26 19:28:35 +0900

    Feat: 채팅방 생성 기능
    채팅방 생성 시 현재 소켓은 채팅방 참여중인 소켓에 추가
    
    동일 유저의 모든 소켓(다른 기기)은 새로 만든 채팅방을 join 및 채팅방 생성을 알림(emit)

--- src/chats/chats.gateway.ts
+++ src/chats/chats.gateway.ts
@@ -7,7 +7,7 @@ import {
   WebSocketServer,
   WsException,
 } from '@nestjs/websockets';
-import { Server, Socket } from 'socket.io';
+import { Namespace, Socket } from 'socket.io';
 import { ChatsService } from './chats.service';
 import { AuthService } from '../auth/auth.service';
 
@@ -15,13 +15,16 @@ import { AuthService } from '../auth/auth.service';
   namespace: 'chats',
 })
 export class ChatsGateway implements OnGatewayConnection {
+  @WebSocketServer()
+  server: Namespace;
+
+  inChatUserMap = {};
+  userSocketMap = {};
+
   constructor(
     private readonly authService: AuthService,
     private readonly chatsService: ChatsService,
   ) {}
-
-  @WebSocketServer()
-  server: Server;
 
   async handleConnection(socket): Promise<any> {
     try {
@@ -35,10 +38,37 @@ export class ChatsGateway implements OnGatewayConnection {
       const payload = this.authService.verifyToken(accessToken);
       socket.user = await this.authService.getUserByPayload(payload);
 
-      await this.chatsService.addUserSocket(socket.user.id, socket.id);
+      await this.chatsService.addUserSocket(
+        this.userSocketMap,
+        socket.user.id,
+        socket.id,
+      );
     } catch (e) {
       socket.emit('disconnected', '유효하지 않은 토큰입니다.');
       socket.disconnect();
     }
+  }
+
+  /**
+   * 채팅방 생성 시 현재 소켓은 채팅방에 접속 유저로 설정
+   * 유저의 모든 소켓(다른 기기)은 새로 만든 채팅방을 join
+   * 모든 소켓에게 채팅방 생성을 알림
+   */
+  @SubscribeMessage('create_room')
+  async createRoom(@ConnectedSocket() socket) {
+    const newRoom = await this.chatsService.createRoom(socket.user);
+
+    await this.chatsService.addInChatUser(
+      this.inChatUserMap,
+      socket,
+      newRoom.id,
+    );
+
+    const userSockets = this.userSocketMap[socket.user.id];
+    userSockets.forEach((socketId) => {
+      const s = this.server.sockets.get(socketId);
+      s.join(newRoom.id.toString());
+      s.emit('create_room', '새로운 채팅방이 생성되었습니다.');
+    });
   }
 }
--- src/chats/wsStatusMap.ts
+++
@@ -1,2 +0,0 @@
-export const inChatUserMap = {};
-export const userSocketMap = {};
