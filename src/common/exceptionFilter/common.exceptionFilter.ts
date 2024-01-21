8dff64b82fe3f802e4d227934c5a5b16f93bc46a
jjoonior <jjoonior97@gmail.com>
2024-01-21 12:30:31 +0900

    Fix: CommonExceptionFilter 에러 status에 따라 분류

--- src/common/exceptionFilter/common.exceptionFilter.ts
+++ src/common/exceptionFilter/common.exceptionFilter.ts
@@ -14,21 +14,26 @@ export class CommonExceptionFilter implements ExceptionFilter {
   constructor(
     @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger,
   ) {}
-  catch(exception: Error, host: ArgumentsHost) {
+  catch(exception: any, host: ArgumentsHost) {
     try {
       const response = host.switchToHttp().getResponse<Response>();
-      const status = HttpStatus.INTERNAL_SERVER_ERROR;
+      const statusCode = exception.status || HttpStatus.INTERNAL_SERVER_ERROR;
+      const message = exception.message || 'INTERNAL_SERVER_ERROR';
 
-      response.status(status).json({
-        statusCode: status,
-        message: 'INTERNAL_SERVER_ERROR',
+      response.status(statusCode).json({
+        statusCode,
+        message,
       });
     } catch (e) {
       this.logger.error(e.stack);
     } finally {
-      // stack의 길이가 너무 길지 않도록 5줄로 제한하여 저장
-      const errorStack = exception.stack.split('\n').slice(0, 5).join('\n');
-      this.logger.error(errorStack);
+      if (!exception.status || exception.status >= 500) {
+        // stack의 길이가 너무 길지 않도록 5줄로 제한하여 저장
+        const errorStack = exception.stack.split('\n').slice(0, 5).join('\n');
+        this.logger.error(errorStack);
+      } else if (exception.status >= 400) {
+        this.logger.warn(exception.message);
+      }
     }
   }
 }
